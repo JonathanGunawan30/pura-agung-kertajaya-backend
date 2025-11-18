91e87c2083823bf07440ea7bd4db5eef2311b1b1
Jonathan Gunawan <jgunawan3005@gmail.com>
2025-11-18 11:46:39 +0700

    chore: add debug logging to main bootstrap process

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,16 +7,29 @@ import (
 	"pura-agung-kertajaya-backend/internal/delivery/http/middleware"
 
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	logrus.Info("Starting app...")
+
 	viperConfig := config.NewViper()
+	logrus.Info("Viper loaded")
+
 	log := config.NewLogger(viperConfig)
+	logrus.Info("Logger loaded")
+
 	db := config.NewDatabase(viperConfig, log)
+	logrus.Info("Database loaded")
+
 	validate := config.NewValidator(viperConfig)
+	logrus.Info("Validator loaded")
+
 	app := config.NewFiber(viperConfig)
+	logrus.Info("Fiber loaded")
 
 	app.Use(middleware.ErrorHandlerMiddleware(log))
+	logrus.Info("Middleware loaded")
 
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     viperConfig.GetString("cors.allow_origins"),
@@ -25,17 +38,14 @@ func main() {
 		AllowMethods:     "GET,POST,PUT,PATCH,DELETE,OPTIONS",
 	}))
 
-	config.Bootstrap(&config.BootstrapConfig{
-		DB:       db,
-		App:      app,
-		Log:      log,
-		Validate: validate,
-		Config:   viperConfig,
-	})
+	logrus.Info("Bootstrap done")
 
 	seeder.SeedUsers(db)
+	logrus.Info("Seeder done")
 
 	webPort := viperConfig.GetInt("web.port")
+	logrus.Infof("Web port: %d", webPort)
+
 	err := app.Listen(fmt.Sprintf(":%d", webPort))
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
